a0dffc2a14e5382918624ca0fb6ca52462f8d749
Ryan Bahniuk <ryan.bahniuk@gmail.com>
2014-10-12 19:17:09 -0500

    testing prime factors

---
+++ prime_factors_runner.rb
@@ -0,0 +1,5 @@
+require_relative 'prime_factors'
+
+puts "prime factors of 8 are #{prime_factors(8)}"
+puts "prime factors of 25 are #{prime_factors(25)}"
+puts "prime factors of 223245 are #{prime_factors(223245)}"
